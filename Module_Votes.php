ea9c49abd8746c703ddc253483e3869c0b22007c
gizmore <gizmore@wechall.net>
2017-07-01 16:26:17 +0200

    sync

--- Module_Votes.php
+++ Module_Votes.php
@@ -1,7 +1,7 @@
 <?php
 final class Module_Votes extends GWF_Module
 {
-	public $module_priority = 40;
+	public $module_priority = 25;
 	public function getClasses() { return ['GWF_VoteTable', 'GWF_VotedObject', 'GDO_VoteSelection', 'GDO_VoteCount', 'GDO_VoteRating']; }
 	
 	public function onIncludeScripts()
